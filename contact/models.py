0c623a1c963215afeb4206830e81595e0b4b54dc
Juan Acosta <acosta.juan@icloud.com>
2025-12-18 15:19:04 -0500

    Finished Contact
    And removed __pycache__ from repository and ignore it

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class ContactMessage(models.Model):
+    """Stores a contact form submission."""
+
+    name = models.CharField(_("name"), max_length=100)
+    email = models.EmailField(_("email"))
+    subject = models.CharField(_("subject"), max_length=200, blank=True)
+    message = models.TextField(_("message"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_resolved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} <{self.email}> - {self.subject}"
