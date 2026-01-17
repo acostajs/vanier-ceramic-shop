93d2bc8a355977f564476e260b59eafc84e3592e
Juan Acosta <acosta.juan@icloud.com>
2026-01-17 15:51:31 -0500

    Adding aws bucket for storing images

--- main/settings.py
+++ main/settings.py
@@ -56,6 +56,16 @@ if os.getenv("RENDER"):
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "vanier-ceramic-shop-images-juan-2026"
+    AWS_S3_REGION_NAME = "us-east-1"
+    AWS_S3_CUSTOM_DOMAIN = (
+        "vanier-ceramic-shop-images-juan-2026.s3.us-east-1.amazonaws.com"
+    )
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 # Middleware
