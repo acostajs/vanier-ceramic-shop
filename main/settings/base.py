2f25029d4aaf3dd306e70cd48d5e5622c57f7759
acostajs <acosta.juan@icloud.com>
2026-06-07 17:53:26 -0400

    Configure MEDIA_URL and MEDIA_ROOT in base.py and clean up development.py

--- main/settings/base.py
+++ main/settings/base.py
@@ -113,6 +113,10 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# Media files base
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Base security (overridden in prod)
