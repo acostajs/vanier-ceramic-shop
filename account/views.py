e27c7e2f52ca3046d29273fd8e88da558de0aaaf
acostajs <acosta.juan@icloud.com>
2026-06-10 15:01:37 -0400

    feat: migrate storage to Cloudinary, fix wishlist bugs, configure and integrate djlint template formatter

--- account/views.py
+++ account/views.py
@@ -99,7 +99,7 @@ def account(request):
 @require_GET
 def wishlist_detail(request):
     """Display the wishlist if the user is logged in."""
-    wishlist = get_object_or_404(Wishlist, account=request.user)
+    wishlist = Wishlist.objects.get_or_create(account=request.user)[0]
     wishlist_products = wishlist.product.all()
 
     context = {
@@ -114,7 +114,7 @@ def wishlist_detail(request):
 def add_to_wishlist(request, product_id):
     """Adds a product to the wishlist if the user is logged in."""
     product = get_object_or_404(Product, pk=product_id)
-    wishlist = get_object_or_404(Wishlist, account=request.user)
+    wishlist = Wishlist.objects.get_or_create(account=request.user)[0]
     wishlist.add(product)
     messages.success(request, f"Added {product.name} to wishlist.")
     return redirect("shop:product", product_id)
@@ -126,7 +126,7 @@ def remove_from_wishlist(request, product_id):
     """Removes a product from the wishlist if the user is logged in."""
     product = get_object_or_404(Product, pk=product_id)
     user = request.user
-    wishlist = get_object_or_404(Wishlist, account=user)
+    wishlist = Wishlist.objects.get_or_create(account=user)[0]
     wishlist.remove(product)
     messages.info(request, f"Removed {product.name} from wishlist.")
     return redirect("account:wishlist_detail")
@@ -136,7 +136,7 @@ def remove_from_wishlist(request, product_id):
 @require_POST
 def clear_wishlist(request):
     """Clear the wishlist from all products if the user is logged in."""
-    wishlist = get_object_or_404(Wishlist, account=request.user)
+    wishlist = Wishlist.objects.get_or_create(account=request.user)[0]
     wishlist.clear()
     msg = _("Wishlist cleared.")
     messages.info(request, msg)
@@ -148,8 +148,8 @@ def clear_wishlist(request):
 def transfer_to_cart(request: HttpRequest, product_id: int) -> HttpResponse:
     """Transfer all the products in the wishlist to the user account related cart."""
     account = request.user
-    wishlist = get_object_or_404(Wishlist, account=account)
-    cart = get_object_or_404(Cart, account=account)
+    wishlist = Wishlist.objects.get_or_create(account=account)[0]
+    cart = Cart.objects.get_or_create(account=account)[0]
     product = get_object_or_404(wishlist.product, pk=product_id)
     cart.add(product, quantity=1, replace=True)
     wishlist.remove(product)
